7c59343043cdaef81b3a709e3489ecaec861f918
Jakob Einarssønn Lunde <jakoel15@uia.no>
2018-05-04 12:19:21 +0200

    noe fremskritt er gjort

--- lib/Pose/src/Pose.cpp
+++ lib/Pose/src/Pose.cpp
@@ -1,56 +1,30 @@
 #include "Pose.h"
 
 Pose::Pose()
-:_sensitivity(16384)
-{}
-
-void Pose::input1(int16_t x, int16_t y, int16_t z)
 {
-  _ref1[0] = x;
-  _ref1[1] = y;
-  _ref1[2] = z;
+
 }
 
-void Pose::input2(int16_t x, int16_t y, int16_t z)
+void Pose::convertToUnitVector(float * rawVector)
 {
-  _ref2[0] = x;
-  _ref2[1] = y;
-  _ref2[2] = z;
-}
-
-float Pose::diffX()
-{
-  _diff[0] = (static_cast<float>(_ref1[0]) - static_cast<float>(_ref2[0]));
-  return _diff[0];
-}
-float Pose::diffY()
-{
-  _diff[1] = (static_cast<float>(_ref1[1]) - static_cast<float>(_ref2[1]));
-  return _diff[1];
-}
-float Pose::diffZ()
-{
-  _diff[2] = (static_cast<float>(_ref1[2]) - static_cast<float>(_ref2[2]));
-  return _diff[2];
-}
-
-void Pose::setSensitivity(uint8_t fs)
-{
-  switch (fs)
+  float vectorLength = sqrt(pow(rawVector[0], 2) + pow(rawVector[1], 2) + pow(rawVector[2], 2));
+  for(uint8_t i = 0; i < 3; ++i)
   {
-    case 0:
-      _sensitivity = 16384;
-      break;
-    case 1:
-      _sensitivity = 8192;
-      break;
-    case 2:
-      _sensitivity = 4096;
-      break;
-    case 3:
-      _sensitivity = 2048;
-      break;
-    default:
-      _sensitivity = 16384;
+    rawVector[i] *= 1/vectorLength;
+  }
+}
+
+void Pose::imuInput(const float * imuVector)
+{
+  float tempVector = imuVector;
+  convertToUnitVector(tempVector);
+  _moduleVector[0] = 0;
+  _moduleVector[1] = 0;
+  _moduleVector[2] = 0;
+  for(uint8_t i = 0; i < 3; ++i)
+  {
+    _moduleVector[0] += _transformationToModule[0][i] * tempVector[i];
+    _moduleVector[1] += _transformationToModule[1][i] * tempVector[i];
+    _moduleVector[2] += _transformationToModule[2][i] * tempVector[i];
   }
 }
